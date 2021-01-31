033154b45c524e87114599edb487e77c54b9ca13
Fabian Williams <fabianwilliams@users.noreply.github.com>
2021-01-31 16:23:31 -0500

    Initial Commit - AzureFunction proj working properly. Now setting up BlazorApp

---
+++ MailbagSeriesFabs/ScopedForUsers/FabsterUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScopedForUsers
+{
+    public class FabsterUser
+    {
+        public string ID { get; set; }
+        public string DisplayName { get; set; }
+        public string GivenName { get; set; }
+        public string EmailAddr { get; set; }
+    }
+}
---
+++ MailbagSeriesFabs/ScopedForUsers/GraphUsers.cs
@@ -0,0 +1,101 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScopedForUsers
+{
+    public static class GraphUsers
+    {
+
+        [FunctionName("GetTopOneUser")]
+        public static async Task<IActionResult> GetTopOneUserFromGraph(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            GraphServiceClient graphClient = GetAuthenticatedGrahClient();
+            List<QueryOption> options = new List<QueryOption>
+            {
+                new QueryOption("$top", "1")
+            };
+
+            var graphResult = graphClient.Users.Request(options).GetAsync().Result;
+
+            log.LogInformation("Graph SDK Result for Top 1 User");
+            log.LogInformation(graphResult[0].DisplayName);
+
+            string responseMessage = string.IsNullOrEmpty(graphResult[0].ToString())
+                ? "Call to Microsoft Graph on Fabster Tenanat App executed successfully."
+                : $"{graphResult[0].DisplayName}";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("GetAllUsers")]
+        public static async Task<IActionResult> GetAllUsersFromGraph(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            List<FabsterUser> ful = new List<FabsterUser>();
+
+            GraphServiceClient graphClient = GetAuthenticatedGrahClient();
+            List<QueryOption> options = new List<QueryOption>
+            {
+                new QueryOption("$select", "displayName,givenName,mail")
+            };
+
+            var graphResult = graphClient.Users.Request(options).GetAsync().Result;
+
+            List<User> usersList = graphResult.CurrentPage.ToList();
+
+            var counter = usersList.Count;
+
+            foreach (User u in usersList)
+            {
+                log.LogInformation("Showing: " + u.GivenName + " - " + u.Mail);
+            }
+
+            log.LogInformation("Graph SDK Result for All Users");
+
+            string responseMessage = string.IsNullOrEmpty(graphResult.ToString())
+                ? "Call to Microsoft Graph on Fabster Tenanat App executed successfully."
+                : $"{graphResult}";
+
+            return new OkObjectResult(usersList);
+        }
+        private static GraphServiceClient GetAuthenticatedGrahClient()
+        {
+            /*
+            var clientId = Environment.GetEnvironmentVariable("AzureADAppClientId", EnvironmentVariableTarget.Process);
+            var tenantID = Environment.GetEnvironmentVariable("AzureADAppTenantId", EnvironmentVariableTarget.Process);
+            var clientSecret = Environment.GetEnvironmentVariable("AzureADAppClientSecret", EnvironmentVariableTarget.Process);
+            */
+            var clientId = "REDACTED";
+            var tenantID = "REDACTED";
+            var clientSecret = "REDACTED";
+
+            // Build a client application.
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                            .Create(clientId)
+                            .WithTenantId(tenantID)
+                            .WithClientSecret(clientSecret)
+                            .Build();
+            ClientCredentialProvider authenticationProvider = new ClientCredentialProvider(confidentialClientApplication);
+            GraphServiceClient graphClient = new GraphServiceClient(authenticationProvider);
+
+            return graphClient;
+        }
+
+    }
+}
